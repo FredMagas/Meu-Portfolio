966dc12d75b0f9d0f04d9fd85a30cf5735117b69
FredMagas <fredericomaga@hotmail.com>
2023-07-29 17:01:09 -0300

    Added domains and chang vercel.json file

--- portfolioweb/portfolioweb/settings.py
+++ portfolioweb/portfolioweb/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app', 'fredericomagalhaes.com.br', 'www.fredericomagalhaes.com.br']
 
 
 # Application definition
