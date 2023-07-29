dda433720ca04314e0b71c699dab5b5710ded69f
FredMagas <fredericomaga@hotmail.com>
2023-07-29 17:50:49 -0300

    Static config changed to deploy

--- portfolioweb/portfolioweb/settings.py
+++ portfolioweb/portfolioweb/settings.py
@@ -123,8 +123,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'fredmagaweb/static')]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
