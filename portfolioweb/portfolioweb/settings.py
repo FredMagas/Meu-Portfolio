f5c64bfbb8457f0b04db9149a8b4e7220978b5ec
FredMagas <fredericomaga@hotmail.com>
2023-07-29 18:05:18 -0300

    Domain modified

--- portfolioweb/portfolioweb/settings.py
+++ portfolioweb/portfolioweb/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app', 'fredericomagalhaes.com.br', 'www.fredericomagalhaes.com.br']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
