8dc808b594359998b85f6f5d8f7f3f77cf3af10b
FredMagas <fredericomaga@hotmail.com>
2023-11-03 09:54:54 -0300

    Visual return of publish and hide PortfolioItem

--- fredmagaweb/admin.py
+++ fredmagaweb/admin.py
@@ -10,3 +10,12 @@ make_unpublished.short_description = "Despublicar casos selecionados"
 def make_published(modeladmin, request, queryset):
     queryset.update(publicado=True)
 make_published.short_description = "Publicar casos selecionados"
+
+class PortfolioItemInline(admin.StackedInline):
+    model = PortfolioItem
+    extra = 1
+
+class CaseAdmin(admin.ModelAdmin):
+    actions = [make_published, make_unpublished]
+    list_display = ('titulo', 'publicado')
+    inlines = [PortfolioItemInline]
