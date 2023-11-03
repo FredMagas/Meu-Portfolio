0ef114b2ee3d1c908f96a804666f1022d9dae97b
FredMagas <fredericomaga@hotmail.com>
2023-11-03 12:40:18 -0300

    Added shadow in header to be more visible

--- portfolioweb/settings.py
+++ portfolioweb/settings.py
@@ -92,10 +92,11 @@ DATABASES = {
     }
 }
 
-DATABASES['default'] = dj_database_url.config(
-    conn_max_age=600,
-    conn_health_checks=True,
-)
+if DEBUG == False:
+    DATABASES['default'] = dj_database_url.config(
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 
 
 # Password validation
