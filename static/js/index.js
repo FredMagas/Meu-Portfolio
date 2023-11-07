507ebb7858dac0226f3d6891eefc8b4987a9a265
FredMagas <fredericomaga@hotmail.com>
2023-11-07 13:53:52 -0300

    Hamburguer icon change when clicked in mobile

--- static/js/index.js
+++ static/js/index.js
@@ -9,16 +9,19 @@ window.onscroll = function() {
 
 var btnPageUp = document.querySelector(".btn-pageup");
 btnPageUp.addEventListener("click", function(event) {
-    event.preventDefault();
-    window.scrollTo(0, 0);
+  event.preventDefault();
+  window.scrollTo(0, 0);
 });
 
-/* Alterne entre adicionar e remover a classe "responsive" para topnav quando o usuário clicar no ícone */
-function myFunction() {
-    var x = document.getElementById("sidebar-menu");
-    if (x.className === "") {
-      x.className += "responsive";
-    } else {
-      x.className = "";
-    }
+function menu() {
+  var x = document.getElementById("sidebar-menu");
+  var icon = document.getElementById("menu-icon");
+
+  if (x.className === "") {
+    x.className += "responsive";
+    icon.className = "fa-solid fa-xmark fa-xl";
+  } else {
+    x.className = "";
+    icon.className = "fa-solid fa-bars fa-xl";
   }
+}
