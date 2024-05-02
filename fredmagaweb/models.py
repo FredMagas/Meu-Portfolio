851f44a486a1c9de358d501ce1af66890c52962b
FredMagas <fredericomaga@hotmail.com>
2024-05-02 00:55:24 -0300

    Change telephone form type to number and limit max length in models.py

--- fredmagaweb/models.py
+++ fredmagaweb/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 class Contato(models.Model):
-    nome = models.CharField(max_length=100)
+    nome = models.CharField(max_length=40)
     email = models.EmailField()
-    empresa = models.CharField(max_length=100)
+    empresa = models.CharField(max_length=40)
     telefone = models.CharField(max_length=20)
     media = models.CharField(max_length=100)
     mensagem = models.TextField(350)
