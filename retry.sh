6584753cf5e823a9cae973e2a8492a139cd6760d
FredMagas <fredericomaga@hotmail.com>
2024-08-08 16:12:00 -0300

    Create a retry.sh script to execute comands for main.yml

---
+++ retry.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+retry() {
+    local n=1
+    local max=5
+    local delay=5
+    while true; do
+        "$@" && break || {
+            if [[ $n -lt $max ]]; then
+                ((n++))
+                echo "Command failed. Attempt $n/$max:"
+                sleep $delay;
+            else
+                echo "The command has failed after $n attempts."
+                return 1
+            fi
+        }
+    done
+}
+
+echo "Running migrations with retries..."
+retry python manage.py makemigrations
+retry python manage.py migrate
