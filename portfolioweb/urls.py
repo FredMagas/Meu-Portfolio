0d3702924ec54f77dd6309b5f62b4915ccfa53b9
FredMagas <fredericomaga@hotmail.com>
2024-08-08 23:45:42 -0300

    Change urls.py urlpatterns for static files

--- portfolioweb/urls.py
+++ portfolioweb/urls.py
@@ -25,4 +25,7 @@ urlpatterns = [
     path('', include('fredmagaweb.urls')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
